ca82a6acd70fe2dd9c07cbcaf54c05c21509b69a
Ярослав Семченко <ukrkyi@gmail.com>
2019-05-19 12:00:53 +0300

    Added images
    Signed-off-by: Ярослав Семченко <ukrkyi@gmail.com>

--- index.js
+++ index.js
@@ -10,7 +10,9 @@ document.body.addEventListener('click', ev => {
     if (ev.target.matches('.Game__StartButton')) {
         button.style.display = 'none';
         document.body.querySelector(".Game__name").style.display = 'none';
-        market = new StockMarket(["Bear", "Grills", "Cat", "Gopher", "Shit"], document.body.querySelector(".Game__market-container"), Array(5).fill("./Stocks/src/img/50-x-50-icon-28.jpg.png"));
+        market = new StockMarket(["Кінь", "Ведмідь", "Вівця", "Кабан", "Бик"], document.body.querySelector(".Game__market-container"),
+            Array.from({length: 4}, (_, i) => "./Evaluation/src/img/row-" + (i+1) + "-col-1.png").
+        concat(["./Evaluation/src/img/icon.png"]));
         let timerPlace = document.body.querySelector(".Game__timer-place");
         timer = new GameTimer(timerPlace);
         let stopGame = document.createElement('button');
@@ -20,7 +22,10 @@ document.body.addEventListener('click', ev => {
             ev.target.style.display = "none";
             //document.body.querySelector(".Game__market-container").style.display ="none";
             document.body.querySelector(".Game__timer-place").style.display = "none";
-            evaluation = new ScoreEvaluation(document.body.querySelector(".Evaluation"), market, [3, 2, 2], Array(5).fill(Array(5).fill("./Evaluation/src/img/icon.png")));
+            evaluation = new ScoreEvaluation(document.body.querySelector(".Evaluation"), market, [5, 4, 3],
+                Array.from({length: 4}, (_, i) =>
+                    Array.from({length: 5}, (_, j) => "./Evaluation/src/img/row-" + (i+1) + "-col-" + (j+1) + ".png"))
+                    .concat([Array(5).fill("./Evaluation/src/img/icon.png")]));
         });
         timerPlace.parentNode.insertBefore(stopGame, timerPlace.nextSibling);
         console.log(market.getPrices());
