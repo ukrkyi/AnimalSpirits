1ab41f212bca5d21b6f970adb18e209e8c174b83
Arattel <sizonov2000@gmail.com>
2019-06-01 14:27:26 +0300

    Added game entering mechanics

--- enter_game.js
+++ enter_game.js
@@ -1,12 +1,23 @@
-
-
-is_valid = async  (game_id) =>{
+is_valid = async (game_id) => {
     let response = await fetch("localhost/games/{game_id}");
-    if(!response.empty){
+    if (!response.empty) {
         return true;
     }
 };
 is_valid_tr = (game_id) => (true);
 
-document.addEventListener()
+const get_id = () => {
+    return document.querySelector(".Login__text-field").value;
+};
+document.addEventListener('click', evt => {
+    if(evt.target.matches(".Login__submit-button")){
+        const id = get_id();
+        if(is_valid_tr(id)){
+            window.location.replace("./index.html");
+        }else{
+            alert("Wrong_id")
+        }
+    }
+
+});
 
